1a8bcaf8015720bb1c47338032c668e0b251cd37
Ninad Sakhardande <ninad.sakhardande@gmail.com>
2018-05-29 11:43:48 +0530

    Added Docstring

---
+++ app.py
@@ -0,0 +1,13 @@
+"""
+	This app stores information of books:
+		Title, Author, Year, ISBN
+
+	User can:
+		View all records
+		Search an entry
+		Add an entry
+		Update an entry
+		Delete an entry
+		Exit App
+
+"""
